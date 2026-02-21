95d1d34a68e9b0c6d3003c3447af53393e0381c3
amanas96 <amanghatampur@gmail.com>
2026-02-21 15:39:44 +0530

    OAuth Link Updated

--- frontend/src/pages/publicPage.jsx
+++ frontend/src/pages/publicPage.jsx
@@ -89,7 +89,7 @@ export default function PublicPage() {
         </div>
 
         <a
-          href="https://sydney-event-backend.onrender.com/auth/google"
+          href="https://sydney-events-0qvx.onrender.com/auth/google"
           className="text-sm font-bold px-6 py-2.5 border-2 border-black rounded-full hover:bg-black hover:text-white transition-all whitespace-nowrap"
         >
           Admin Login
